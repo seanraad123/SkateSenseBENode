e3b298e18ae88b97fa5946e648db858efa408244
seanrad <seanconrad123@gmail.com>
2019-11-21 21:55:17 -0500

    added delete bookmark

--- graphql/resolvers/deleteBookmark.js
+++ graphql/resolvers/deleteBookmark.js
@@ -3,14 +3,26 @@ const User = require('../../models/user')
 
 
 module.exports = async function deleteBookmark({ bookmarkInput }, req) {
-  console.log(req);
-  
 
-    // deconstruct
-  const { spot_id } = bookmarkInput;
+  // deconstruct
+  const { spot_id, user_id } = bookmarkInput;
 
+  const userToUpdate = await User.findOne({_id: user_id })
 
- 
+  let index = userToUpdate.bookmarks.indexOf(spot_id);
 
-  return 'Bookmark was deleted';
+  console.log('WAHT IS INDEX', userToUpdate)
+
+  let newArray = []
+  if (index >= 0) {
+    newArray = userToUpdate.bookmarks.splice( index, 1 );
+  }
+
+  // const user = User.findOne({_id: user_id})
+  const user = User.findOneAndUpdate(
+    { _id: user_id },
+    { $set: { bookmarks: newArray } }
+  )
+
+  return user
 };
